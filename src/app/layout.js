770c7b74cad9476e0755fa7b56b0ee474e6af3cd
Bilkis Ismail <bilkisismail07@gmail.com>
2025-07-21 22:03:15 +0700

    Add TypeScript support, refactor layout components, and remove unused files

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,24 @@
 import * as React from "react";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import { ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-import theme from "@/lib/mui/theme";
 import { StoreProvider } from "./StoreProvider";
+import theme from "@/lib/mui/theme";
+import CssBaseline from "@mui/material/CssBaseline";
 import Main_layout from "@/components/layout/_main.layout";
+import Empty_layout from "@/components/layout/_empty.layout";
 import "@/styles/global.css"; // Ensure global styles are imported
 
 export default function RootLayout(props) {
+  const Layout = (layout) => {
+    switch (layout) {
+      case value:
+        return <Empty_layout>{props.children}</Empty_layout>;
+
+      default:
+        return <Main_layout>{props.children}</Main_layout>;
+    }
+  };
+
   return (
     <html lang="en">
       <body>
@@ -16,7 +27,7 @@ export default function RootLayout(props) {
             <ThemeProvider theme={theme}>
               {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
               <CssBaseline />
-              <Main_layout>{props.children}</Main_layout>
+              <Layout layout={props.layout} />
             </ThemeProvider>
           </AppRouterCacheProvider>
         </StoreProvider>
---
+++ src/components/layout/_empty.layout.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Empty_layout() {
+  return <></>;
+}
+
+export default Empty_layout;
