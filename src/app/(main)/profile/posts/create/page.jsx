67f4129c40715857ea60b4c1cc0a658aaf31319a
kubil <bilkisismail07@gmail.com>
2025-08-21 19:24:35 +0700

    update

--- src/app/(main)/profile/posts/create/page.jsx
+++ src/app/(main)/profile/posts/create/page.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import Post_create_pages from "@/components/pages/(main)/post/create/post.create.pages";
+import http from "@/lib/axios/http-server";
 
-function Page() {
-  return <Post_create_pages />;
+async function Page() {
+  const request = await http.post("/videos/mux");
+  console.log(request)
+  return <Post_create_pages uploadUrl={request?.data?.url} id={request?.data?.id} />;
 }
 
 export default Page;
